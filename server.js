c51a31108ab2f31273ddc69dcebd4a0d5ad94fe0
Alhassane Seye <seyealhassane@gmail.com>
2023-11-03 22:00:26 +0100

    Notes

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 const http = require('http');
 const app = require('./app');
 
-const normalizePort = val => {
+const normalizePort = val => { // Renvoie un port valide, qu'il soit fourni sous la forme d'un numero ou d'une chaine
     const port = parseInt(val, 10);
-  
     if (isNaN(port)) {
       return val;
     }
@@ -12,11 +11,10 @@ const normalizePort = val => {
     }
     return false;
   };
-
 const port = normalizePort(process.env.PORT || '4000');
 app.set('port',port)
 
-const errorHandler = error => {
+const errorHandler = error => { // Recherche les differentes erreurs et les gere de maniere appropriee. La fonction est ensuite enregistree dans le serveur
     if (error.syscall !== 'listen') {
       throw error;
     }
@@ -38,7 +36,7 @@ const errorHandler = error => {
 
 const server = http.createServer(app);
 
-server.on('error', errorHandler);
+server.on('error', errorHandler); //  Ecouteur d'evenements, consigne le port nommé sur lequel le serveur s'execute dans la console.
 server.on('listening', () => {
   const address = server.address();
   const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
